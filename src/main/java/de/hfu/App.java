d6646b559c398db5bcda5eeda2218e7f3f7c401f
Hergerta <alexander.hergert@hs-furtwangen.de>
2022-11-24 11:05:42 +0100

    erstellt

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,13 @@
+package de.hfu;
+#include java.util.scanner;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(new Scanner scanner( ).nextLine.toUpperCase());
+    }
+}
